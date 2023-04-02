9e8bc12a60d8a82f3ab52bb287502957bf8d49d6
tanyarubi <67060601+tanyarubi@users.noreply.github.com>
2023-04-02 15:21:24 +0300

    Update STIAT-complexity.js

--- STIAT-complexity.js
+++ STIAT-complexity.js
@@ -43,7 +43,7 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/stiat/qua
 				{word: 'peter'},
 				{word: 'alan'},
 				{word: 'robert'},
-				{word: 'daniel'}
+
 				
 			], 
 			//Can change color and size of the targets here.
@@ -66,7 +66,6 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/stiat/qua
 				{word: 'gwen'},
 				{word: 'sara'},
 				{word: 'diane'},
-			  	{word: 'karen'},
 				{word: 'laurel'},
 				{word: 'eva'},
 				{word: 'sandra'},
